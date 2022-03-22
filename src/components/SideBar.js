b3632f46d26884e38ff702f62e885ae1964fd1d4
Worash <uppert83@gmail.com>
2022-03-22 16:56:25 +0300

    add store,brand,supplier,customer link  to sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,36 +1,53 @@
 import React from "react";
-import  {Menu} from 'antd'
+import  {Menu,Avatar } from 'antd'
 import {Link} from 'react-router-dom'
-
-
-
+import {
+    HomeOutlined, AlibabaOutlined,
+    SafetyCertificateOutlined,
+    ClockCircleOutlined,
+    CheckSquareOutlined,
+    BookOutlined,
+    CarryOutOutlined,
+    AppstoreOutlined 
+} from '@ant-design/icons'
+import './sideBar.css'
+import logo2 from '../images/logo2.png'
 export const SideBar =()=>{
 
 
 
     return(
-    <>
-        <Menu  mode="inline" theme ="dark" className="menu">
-            <Menu.Item key="/" className="menu-item" >
-            <Link to="/" className="menu-link" style ={{color:"white", fontSize:"25px"}}>Button P</Link>
+        <>
+        <div className="logo" >
+         <Link to ="/">    <img src={logo2} />
+                <h1>OIMS</h1>
+         </Link>
+           </div>
+            
+        <Menu  mode="inline" theme ="light" className="menu">
+                <Menu.Item key="/" className="menu-item" icon={<HomeOutlined style={ {color:"#fff",fontSize:20}}/>} >
+            <Link to="/" className="menu-link" >Home</Link>
             </Menu.Item>
-            <Menu.Item key="grid" className="menu-item">
-            <Link to="/grid"  className="menu-link" style ={{color:"white", fontSize:"25px"}}> Grid Practice</Link>
+            <Menu.Item key="company" className="menu-item" icon ={<AlibabaOutlined style={ {color:"#fff",fontSize:30}} />}>
+            <Link to="/company"  className="menu-link" >Company</Link>
             </Menu.Item>
-            <Menu.Item key="laylout" className="menu-item">
-            <Link to="/layout"  className="menu-link" style ={{color:"white", fontSize:"25px"}}> Layout Practice</Link>
+            <Menu.Item key="Category" className="menu-item" icon ={<SafetyCertificateOutlined  style={ {color:"#fff",fontSize:20}}/>}>
+            <Link to="/Category"  className="menu-link" >Categories</Link>
             </Menu.Item>
-            <Menu.Item key="menu" className="menu-item">
-            <Link to="/menu"  className="menu-link" style ={{color:"white", fontSize:"25px"}}> Menu Practice</Link>
+            <Menu.Item key="brand" className="menu-item" icon ={<ClockCircleOutlined  style={ {color:"#fff",fontSize:20}}/> }>
+            <Link to="/brand"  className="menu-link" > Brands</Link>
+                </Menu.Item>
+                <Menu.Item key="store" className="menu-item" icon ={<AppstoreOutlined   style={ {color:"#fff",fontSize:20}}/> }>
+            <Link to="/store"  className="menu-link" > Store</Link>
             </Menu.Item>
-            <Menu.Item key="dropdown" className="menu-item">
-            <Link to="/table"  className="menu-link" style ={{color:"white", fontSize:"25px"}}> Table Practice</Link>
+            <Menu.Item key="supplier" className="menu-item" icon ={<CheckSquareOutlined style={ {color:"#fff",fontSize:20}}/>}>
+            <Link to="/supplier"  className="menu-link"> Suppliers</Link>
             </Menu.Item>
-            <Menu.Item key="form" className="menu-item" style ={{marginLeft:"20px"}}>
-            <Link to="/form"  className="menu-link" style ={{color:"white", fontSize:"25px"}}> Form Practice</Link>
+            <Menu.Item key="customer" className="menu-item" icon ={<BookOutlined style={ {color:"#fff",fontSize:20}}/>}>
+            <Link to="/customer"  className="menu-link" > Customers</Link>
             </Menu.Item>
-            <Menu.Item key="input" className="menu-item">
-            <Link to="/input"  className="menu-link" style ={{color:"white", fontSize:"25px"}}> Input Practice</Link>
+            <Menu.Item key="user" className="menu-item" icon ={<CarryOutOutlined  style={ {color:"#fff",fontSize:20}}/>}>
+            <Link to="/user"  className="menu-link" > Users</Link>
             </Menu.Item>
         </Menu>
     </>

07097993a3bce096e97016d5c0a52dc7b022f122
Worash <uppert83@gmail.com>
2022-03-22 17:24:57 +0300

    customer page added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CategoryPage from './pages/CategoryPage';
 import BrandPage from './pages/BrandPage'
 import StorePage from './pages/StorePage';
 import SupplierPage from './pages/SupplierPage';
-
+import CustomerPage from './pages/CustomerPage';
 import  {  BrowserRouter as Router,
   Routes,
   Route,
@@ -33,7 +33,9 @@ function App() {
          <Route path="/supplier" element={<SupplierPage/>}>
          
          </Route>
-        
+         <Route path="/customer" element={<CustomerPage/>}>
+         
+         </Route>
           </Routes>
 
 

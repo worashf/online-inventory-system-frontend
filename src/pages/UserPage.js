0bc322f3dee801ee3be47ae9199853761b3bcc00
worashf <uppert83@gmail.com>
2022-03-23 02:21:20 +0300

    user page added

---
+++ src/pages/UserPage.js
@@ -0,0 +1,43 @@
+
+import React from  'react'
+import {Layout} from 'antd'
+import { SideBar } from '../components/SideBar'
+import { HeaderBar } from '../components/HeaderBar'
+import { FooterBar } from '../components/FooterBar'
+import   UserTable from '../components/table/UserTable'
+
+
+const {Header,Content,Sider,Footer} = Layout
+
+
+
+const UserPage =() =>{
+    return(
+        <>
+
+<Layout className="site-layout">
+                <Sider theme="light" style={{borderRight:"1px solid #111"}}>
+            
+                
+                      <SideBar/>
+            </Sider>
+        <Layout>
+          <Header  style={{ background:"#997105",height:100,textAlign:"center"
+          ,paddingTop:20}}>
+               <HeaderBar/>
+             </Header>
+            <Content style={{ with: "100%", height: 500 }}>
+                      
+                      <UserTable />
+                    </Content>
+          <Footer style={{background:"#997105",}}>
+              <FooterBar/>
+          </Footer>
+        </Layout>
+            </Layout>
+        </>
+    )
+}
+
+
+export default UserPage;

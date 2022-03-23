ca01ffc1508be7f896990f60906bfbb6b872e61d
worashf <uppert83@gmail.com>
2022-03-24 02:09:54 +0300

    add brand page as child

--- src/pages/BrandPage.js
+++ src/pages/BrandPage.js
@@ -1,41 +1,18 @@
 
 import React from  'react'
-import {Layout} from 'antd'
-import { SideBar } from '../components/SideBar'
-import { HeaderBar } from '../components/HeaderBar'
-import { FooterBar } from '../components/FooterBar'
+import ItemContainer from './item.container'
 import BrandTable from '../components/table/BrandTable'
 
 
-const {Header,Content,Sider,Footer} = Layout
+
 
 
 
 const CategoryPage =() =>{
     return(
-        <>
-
-<Layout className="site-layout">
-                <Sider theme="light" style={{borderRight:"1px solid #111"}}>
-            
-                
-                      <SideBar/>
-            </Sider>
-        <Layout>
-          <Header  style={{ background:"#997105",height:100,textAlign:"center"
-          ,paddingTop:20}}>
-               <HeaderBar/>
-             </Header>
-            <Content style={{ with: "100%", height: 500 }}>
-                      
-                      <BrandTable />
-                    </Content>
-          <Footer style={{background:"#997105",}}>
-              <FooterBar/>
-          </Footer>
-        </Layout>
-            </Layout>
-        </>
+        <ItemContainer>
+            <BrandTable/>
+        </ItemContainer>
     )
 }
 

5c2e3e172f30251c8bedaf2bb4a961e9a22ea4c6
worashf <uppert83@gmail.com>
2022-03-23 22:19:08 +0300

    modified

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -5,6 +5,7 @@ import { SideBar } from '../components/SideBar'
 import { HeaderBar } from '../components/HeaderBar'
 import { FooterBar } from '../components/FooterBar'
 import CategoryTable from '../components/table/CategoryTable'
+import ItemContainer from './item.container'
 
 
 const {Header,Content,Sider,Footer} = Layout
@@ -13,29 +14,9 @@ const {Header,Content,Sider,Footer} = Layout
 
 const CategoryPage =() =>{
     return(
-        <>
-
-<Layout className="site-layout">
-                <Sider theme="light" style={{borderRight:"1px solid #111"}}>
-            
-                
-                      <SideBar/>
-            </Sider>
-        <Layout>
-          <Header  style={{ background:"#997105",height:100,textAlign:"center"
-          ,paddingTop:20}}>
-               <HeaderBar/>
-             </Header>
-            <Content style={{ with: "100%", height: 500 }}>
-                      
-                      <CategoryTable />
-                    </Content>
-          <Footer style={{background:"#997105",}}>
-              <FooterBar/>
-          </Footer>
-        </Layout>
-            </Layout>
-        </>
+        <ItemContainer>
+              <CategoryTable />
+        </ItemContainer>
     )
 }
 

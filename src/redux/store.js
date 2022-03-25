cb793f03953bc556c38f0efb864b04a19bbef26b
Worash <uppert83@gmail.com>
2022-03-25 17:16:29 +0300

    redux state store added

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+
+import rootReducer from "./rootReducer";
+
+import { createStore } from "redux";
+
+
+
+const store = createStore(rootReducer,
+    {},
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+export default store;

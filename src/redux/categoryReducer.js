4d9bf140063cce9fd784fcb833f7414cc8c03830
Worash <uppert83@gmail.com>
2022-03-26 17:22:19 +0300

    modify category reducer

--- src/redux/categoryReducer.js
+++ src/redux/categoryReducer.js
@@ -6,14 +6,16 @@ const initialState =[ ]
 export const categoryReducer = (state = initialState, { type, payload }) => {
     switch (type) {
         case actionType.ADD_CATEGOTY:
-            return [...state, payload]
+            return  [...state,payload];
         case actionType.LIST_CATEGORIES:
-            return [...state,payload]
+            return payload;
                 
         case actionType.DELETE_CATEGORY:
-            const currentCategories = state.filter((category) => category.id === payload.id ? null : category);
+            const currentCategories = state.filter((category) => category.categoryId === payload ? null : category);
             return currentCategories;
-            
+        case actionType.UPDATE_CATEGORY:
+            return  [...state,payload];
+
         default:
             return state
  }

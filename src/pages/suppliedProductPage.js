7bed23739b075446a9120488417a4519dcf7fc5c
worashf <uppert83@gmail.com>
2022-05-07 22:33:36 +0300

    add supplied product page

---
+++ src/pages/suppliedProductPage.js
@@ -0,0 +1,18 @@
+import React from  'react'
+import ItemContainer from './item.container'
+import SuppliedProduct from '../components/inventory/SuppliedProduct'
+
+
+
+
+
+const SuppliedProductPage =() =>{
+    return(
+        <ItemContainer>
+         <SuppliedProduct/>
+        </ItemContainer>
+    )
+}
+
+
+export default SuppliedProductPage ;

46e84d80398fcd5822279f6b68c443149115a2d5
worashf <uppert83@gmail.com>
2022-05-07 22:33:09 +0300

    add inventory trasfer page

---
+++ src/pages/TransferInventoryPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import TransferInventory from '../components/inventory/TransferInventory';
+import ItemContainer from './item.container';
+
+const TransferInventoryPage =()=>{
+    return(
+<>
+<ItemContainer>
+<TransferInventory/>
+</ItemContainer>
+
+</>
+    )
+}
+
+export default TransferInventoryPage;
